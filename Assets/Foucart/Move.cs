f7460813aef734648dd177c1a5e3d1b7d7b660c8
Foucart <thomas.foucart@outlook.fr>
2021-05-26 12:37:06 +0100

    ajout dialogue , quetes

--- Assets/Foucart/Move.cs
+++ Assets/Foucart/Move.cs
@@ -7,9 +7,14 @@ public class Move : MonoBehaviour
         new Rigidbody2D rigidbody2D;
 
     public float movementSpeed = 1000.0f;
+    public GameObject DialogueCanvas;
+    public bool canMove;
+    public bool DansZoneDialog;
+    public bool Entraindeparler;
 
     void Awake()
     {
+        canMove=true;
         // Setup Rigidbody for frictionless top down movement and dynamic collision
         rigidbody2D = GetComponent<Rigidbody2D>();
 
@@ -20,16 +25,41 @@ public class Move : MonoBehaviour
 
     void Update()
     {
-        // Handle user input
-        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(canMove)
+        {
+             Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         MoveF(targetVelocity);
+        }
+
+        if(DansZoneDialog==true&&Input.GetKey(KeyCode.E))
+        {
+            DialogueCanvas.SetActive(true);
+            canMove=false;
+            Entraindeparler=true;
+        }
+       
     }
 
     void MoveF(Vector2 targetVelocity)
     {        
         // Set rigidbody velocity
         rigidbody2D.velocity = (targetVelocity * movementSpeed) * Time.deltaTime; // Multiply the target by deltaTime to make movement speed consistent across different framerates
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag=="Dialog")
+        {
+            DansZoneDialog=true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.tag=="Dialog")
+        {
+            DansZoneDialog=false;
+        }
+
     }
 }
 
---
+++ Assets/Foucart/Quest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest : MonoBehaviour
+{
+    public GameObject QueteSymbole;
+    public float NumeroQuete;
+    public GameObject Quete1;
+    public GameObject Singe;
+    public Text EtatdelaQuete;
+    public Text DescriptionQuete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quete()
+    {
+        StartCoroutine(QueteAnim());
+    }
+
+
+     IEnumerator QueteAnim()
+    {
+        if(NumeroQuete==1)
+        {
+            Quete1.SetActive(true);
+            Singe.SetActive(true);
+        }
+
+        QueteSymbole.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        QueteSymbole.SetActive(false);
+       
+    }
+    public void QueteTermine()
+    {
+        if(NumeroQuete==1)
+        {
+            EtatdelaQuete.color=Color.green;
+            EtatdelaQuete.text="terminé ";
+        }
+    }
+}
