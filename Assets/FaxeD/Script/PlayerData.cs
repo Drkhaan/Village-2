9c13c5bc8038fb01be3ebbd1b6a5eca14f98c1f1
Drkhaan <maxime.lorin@e-artsup.com>
2021-06-02 18:53:28 +0200

    ah ouais

---
+++ Assets/FaxeD/Script/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerData
+{
+    public float[] position;
+    public string nomJoueur;
+
+    public PlayerData (PlayerName nom)
+    {
+        nomJoueur = nom.saveName;
+    }
+
+    public PlayerData (CharacterMovementPlatformer pos)
+    {
+        position = new float[3];
+        position[0] = pos.transform.position.x;
+        position[1] = pos.transform.position.y;
+        position[2] = pos.transform.position.z;
+    }
+    
+}
---
+++ Assets/FaxeD/Script/Save.cs
@@ -0,0 +1,74 @@
+﻿
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class save
+{
+    public static void SavePlayer (CharacterMovementPlatformer player)
+{
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }   else 
+        {
+            Debug.Log("pas de sauvegarde bogosse"+path);
+            return null;
+        }
+    }
+
+
+    public static void SavePlayer2 (PlayerName player2)
+{
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player2.namesaved";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player2);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+     public static PlayerData LoadPlayer2()
+    {
+        string path = Application.persistentDataPath + "/player2.namesaved";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }   else 
+        {
+            Debug.Log("pas de sauvegarde bogosse"+path);
+            return null;
+        }
+    }
+
+}
+
