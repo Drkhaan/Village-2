3e49be5a402663405b64add40fc6dac51311908a
Théo <theo.dueymes@e-artsup.net>
2021-06-07 16:15:37 +0200

    menu skin

---
+++ Assets/Mambio/Scripts/SkinManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class SkinManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerSkin;
+
+
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin +1;
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+    }
+
+
+    public void BackOption()
+    {
+        selectedSkin = selectedSkin -1;
+        if( selectedSkin < 0)
+        {
+            selectedSkin = skins.Count -1;
+        }
+        sr.sprite = skins[selectedSkin];
+    }
+
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerSkin, "Assets/selectedskin.prefab");
+        SceneManager.LoadScene("Mambio");
+    }
+}
