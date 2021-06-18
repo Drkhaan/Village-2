1c935159d498896b67b0e10afd229ecd2d8f6eb9
Drkhaan <maxime.lorin@e-artsup.com>
2021-06-18 19:09:48 +0200

    prefabs + layers + nouvelles assets

---
+++ Assets/Bilou/PositionRendererSorter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+   [SerializeField]
+   private int sortingOrderBase = 5;
+   private Renderer myRenderer;
+
+   //marche aussi sans ces trucs
+   [SerializeField]
+   private int offset = 0;
+
+
+    private void Awake()
+    {
+        myRenderer =gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+    }
+}
